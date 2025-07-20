e1477d9c25346d4ba19f013cd3a1fff2bd6d6924
unknown <dhanavarshinipr@gmail.com>
2025-07-20 19:21:59 +0530

    image

--- app/components/Features.js
+++ app/components/Features.js
@@ -110,12 +110,7 @@ export default function Features() {
                 img: '/web.jpeg',
                 bg: '#cffafe',
               },
-              {
-                title: 'Designing',
-                content: 'We provide expert design services that ensure your digital platforms are visually appealing, user-centric, and aligned with your brand\'s goals for optimal customer interaction.',
-                img: '/app.jpeg',
-                bg: '#fce7f3',
-              },
+             
               {
                 title: 'Custom AI Solutions',
                 content: 'We provide bespoke AI systems tailored to your business needs, enhancing efficiency, performance, and driving continuous innovation.',
